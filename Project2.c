05af78a427834b0f7da56bb8831b51fddcfc9f0f
Jiya Prajapati <jesskp0886@gmail.com>
2026-01-07 12:02:00 +0530

    Add Project2.c

---
+++ Project2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+    int score;
+    char grade;
+
+    printf("Enter your score: ");
+    scanf("%d", &score);
+
+    /* Step 1: Grade calculation using ternary operator */
+    grade = (score >= 90) ? 'A' :
+            (score >= 80) ? 'B' :
+            (score >= 70) ? 'C' :
+            (score >= 60) ? 'D' : 
+                            'F';
+
+    printf("Your grade is %c", grade);
+
+    /* Step 2: Additional comments using switch-case */
+    switch(grade){
+        case 'A':
+            printf(". Excellent work!\n");
+            break;
+        case 'B':
+            printf(". Well done!\n");
+            break;
+        case 'C':
+            printf(". Good job!\n");
+            break;
+        case 'D':
+            printf(". You passed, but you could do better.\n");
+            break;
+        case 'F':
+            printf(". Sorry, you failed.\n");
+            break;
+    }
+
+    /* Step 3: Eligibility check using if-else */
+    if(grade >= 'A' && grade <= 'D'){
+        printf("You are eligible for the next level.\n");
+    }
+    else{
+        printf("Please try again next time.\n");
+    }
+
+    return 0;
+}
